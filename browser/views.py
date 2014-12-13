891d372082bc0a1fae14cda5b9a2c0f71125e8a6
Jeremy Kun <jkun2@uic.edu>
2014-12-12 20:43:58 -0600

    Added HTML and CSS

--- browser/views.py
+++ browser/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Item
 
-# Create your views here.
+
+def frontPage(request):
+   items = Item.objects.all().order_by('minAge')
+   return render(request, 'browser/frontPage.html', {'items': items})
